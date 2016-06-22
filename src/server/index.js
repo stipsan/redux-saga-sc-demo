55155e8f6a96f0c5a6db68f07c3e3bb7f1af1546
Stian Didriksen <stipsan@gmail.com>
2016-06-22 19:48:59 +0200

    server entry point

---
+++ src/server/index.js
@@ -0,0 +1,17 @@
+const path = require('path')
+const SocketCluster = require('socketcluster').SocketCluster
+
+new SocketCluster({
+  // depending on the Heroku dyno this may vary
+  workers: Number(process.env.WEB_CONCURRENCY) || 1,
+  // heroku sets the port used on the intranet
+  port: process.env.PORT || 5000,
+  // the uws WebSockets engine is faster by many orders of magnitude
+  wsEngine: 'uws', //
+  // everything goes through redux stores
+  allowClientPublish: false,
+  // generated automatically if using the Heroku deploy button
+  authKey: process.env.AUTH_KEY,
+  // this process respawns automatically on crash
+  workerController: path.resolve(__dirname, 'worker.js'),
+})
