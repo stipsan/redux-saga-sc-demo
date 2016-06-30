210b6ed58f50f27da1019e91ca2af4b573dc1b1e
Stian Didriksen <stipsan@gmail.com>
2016-06-30 03:13:10 +0200

    massive performance boost in v2

--- src/client/sagas/index.js
+++ src/client/sagas/index.js
@@ -6,7 +6,6 @@ const socketCluster = global.WebSocket ? require('socketcluster-client') : false
 const socket = socketCluster && socketCluster.connect({
   hostname: process.env.SOCKET_HOSTNAME || location.hostname,
   autoReconnect: true,
-  autoReconnectOptions: process.env.AUTO_RECONNECT_OPTIONS,
   ackTimeout: 10000, // server should never take this long to ping back
 })
 
