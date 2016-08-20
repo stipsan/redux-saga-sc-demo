cba36acee89c03bb6b9f9f437e1231f2e7aa722f
Stian Didriksen <stipsan@gmail.com>
2016-08-20 22:47:10 +0200

    switched to react-virtualized

--- src/client/components/Messages.js
+++ src/client/components/Messages.js
@@ -1,9 +1,13 @@
+import 'react-virtualized/styles.css';
+
 import './Messages.css'
 
 import leftPad from 'left-pad'
 import scrollIntoViewIfNeeded from 'scroll-into-view-if-needed'
 import uikit from 'uikit/dist/css/uikit.almost-flat.css'
 import Remarkable from 'remarkable'
+import { Component } from 'react'
+import { AutoSizer, CellMeasurer, VirtualScroll } from 'react-virtualized'
 
 import Form from './Form'
 
@@ -12,27 +16,8 @@ const md = new Remarkable({
   linkTarget: 'new',
 })
 
-const supportSticky = (function () {
-    var elem = document.createElement('div');
-    elem.style.cssText = 'position:-webkit-sticky';
-    if (elem.style.position.match('-webkit-sticky')) return true;
-    elem.style.cssText = 'position:sticky';
-    if (elem.style.position.match('sticky')) return true;
-    return false;
-}());
-
 const wrapperStyle = Object.freeze({
-  height: !supportSticky && '100vh',
-})
-
-const listStyle = Object.freeze({
-  overflow: !supportSticky && 'auto',
-  WebkitOverflowScrolling: 'touch',
-  marginBottom: '0px',
-  boxSizing: 'border-box',
-  paddingTop: '15px',
-  paddingBottom: '0px',
-  minHeight: supportSticky && '100vh',
+  height: '100vh',
 })
 
 const timeAgoFormatter = (value, unit) => {
@@ -42,30 +27,12 @@ const timeAgoFormatter = (value, unit) => {
 
 let throttle = false
 
-let listClassName = 'uk-list uk-list-line uk-margin-left uk-margin-right'
-if(supportSticky) {
-  listClassName += ' uk-flex-item-none'
-}
+export default class Messages extends Component {
+  renderMessage = ({ index }) => {
+    const payload = this.props.messages[index]
 
-const Messages = ({
-  messages,
-  send
-}) => (
-  <div
-    className="uk-flex uk-flex-column uk-flex-space-between"
-    style={wrapperStyle}
-  >
-    <ul ref={(node) => {
-      if(node && node.childNodes.length > 0) {
-        clearTimeout(throttle)
-        throttle = setTimeout(() => {
-          scrollIntoViewIfNeeded(node.childNodes[node.childNodes.length - 1], false, {
-            duration: 150
-          })
-        }, 10)
-      }
-    }} className={listClassName} style={listStyle}>
-      {messages.map(payload => <li key={payload.id}>
+    return (
+      <div className="tm-message">
         <span
           style={{display: 'inline-block', width: '70px', opacity: .5}}
           title={payload.when.toLocaleString()}
@@ -76,10 +43,45 @@ const Messages = ({
         </span>
         <strong style={{color: payload.color}}>{payload.username} </strong>
         <span dangerouslySetInnerHTML={{__html: md.render(payload.message)}} />
-      </li>)}
-    </ul>
-    <Form onSubmit={send} />
-  </div>
-)
-
-export default Messages
+      </div>
+    )
+  }
+  renderCell = ({ rowIndex }) => {
+    return this.renderMessage({index: rowIndex})
+  }
+  render() {
+    const {messages, send} = this.props
+    console.log(messages, messages.length)
+    return (
+      <div
+        className="uk-flex uk-flex-column uk-flex-space-between"
+        style={wrapperStyle}
+      >
+        <div className="uk-flex-item-auto uk-margin-left uk-margin-right">
+          <AutoSizer>
+            {({ height, width }) => (
+              <CellMeasurer
+                cellRenderer={this.renderCell}
+                columnCount={1}
+                rowCount={messages.length}
+                width={width}
+              >
+                {({ getRowHeight }) => (
+                  <VirtualScroll
+                    height={height}
+                    rowHeight={getRowHeight}
+                    rowCount={messages.length}
+                    rowRenderer={this.renderMessage}
+                    scrollToIndex={Math.max(0, messages.length - 1)}
+                    width={width}
+                  />
+                )}
+              </CellMeasurer>
+            )}
+          </AutoSizer>
+        </div>
+        <Form onSubmit={send} />
+      </div>
+    )
+  }
+}
